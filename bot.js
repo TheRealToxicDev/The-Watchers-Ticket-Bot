b56a5c57eda7c2c5fb9032ff023ecfd95ee949b2
GrimDesigns <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-06-11 17:43:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,7 +111,37 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
         });
     });
 }
+	
+    if(command === "prune") {
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply("**🔒 Sorry, you can't do that.**");
+        var messagesToDelete = args[1];
+        if (!args[1]) return message.channel.send("❌ Please include the amount of Message that you want to **Prune**!");
+        if (args[1] > 99) return message.channel.send("❌ I can't **Prune** more than `99` Messages.");
+        message.channel.fetchMessages({limit: messagesToDelete})
+        .then(messages => message.channel.bulkDelete(messages.size + 1))
+        .catch(error => message.channel.send(`❌ Sorry ${message.author}, Failed while **Prunning** because: *${error}*.`));
+    };
 
+    if(command == "kick") {
+        message.delete()
+        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if(!kUser) return message.channel.send("❌ Please **@mention** your target!");
+        let kReason = args.join(" ").slice(0);
+        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("**🔒 Sorry, you can't do that.**");
+        if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("❌ Failed to **Kick**, need a higher than Roles.");
+    
+        let kickEmbed = new Discord.RichEmbed()
+        .setDescription("**👢 Kicked**")
+        .setColor(0xFF0000)
+        .addField("User", `${kUser}`)
+        .addField("Moderator", `<@${message.author.id}>`)
+        .addField("Reason", `**\`\`\`${kReason}\`\`\`**`);
+    
+        let adminlog = message.guild.channels.find(`name`, "mod-logs");
+        if(!adminlog) return message.channel.send("❌ Sorry, i need the Logging Channels with name **#mod-logs**.");
+        message.guild.member(kUser).kick(kReason);
+        adminlog.send(kickEmbed);
+    };
 });
 
 client.login(process.env.BOT_TOKEN);
