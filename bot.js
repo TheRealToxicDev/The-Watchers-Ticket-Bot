77d907c7462fdb686a6cf62608ed752274a73b17
GrimDesigns <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-06-11 23:20:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,8 @@ client.on("message", (message) => {
     .setTitle(`:mailbox_with_mail: ツ Ticket Bot ツ Ping Command`)
     .setColor(0xCF40FA)
     .addField(`Hold On!!!`)
-    message.channel.send({ embed: embed }); .then(m => {
+    message.channel.send({ embed: embed }); 
+    .then(m => {
     m.edit(`:ping_pong: Wew, we made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
     });
 }
