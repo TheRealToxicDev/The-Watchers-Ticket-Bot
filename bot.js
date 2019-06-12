2e1064c21556c57983842f0984993fb8cf92959e
GrimDesigns <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-06-12 06:58:28 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -195,6 +195,15 @@ function response(c) {
       if(message.channel == c) {
         return message.content;
       }
+function secondsToString(seconds) {
+         let totalSeconds = (client.uptime / 1000);
+         let days = Math.floor(totalSeconds / 86400);
+         let hours = Math.floor(totalSeconds / 3600);
+         totalSeconds %= 3600;
+         let minutes = Math.floor(totalSeconds / 60);
+         let seconds = totalSeconds % 60;
+         let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+      }
     });
   }
 };
