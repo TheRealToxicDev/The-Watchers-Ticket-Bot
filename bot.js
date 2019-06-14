9aa3bee237cc3cfd40efb1c5344993151f83b187
GrimDesigns <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-06-13 21:09:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,6 +71,23 @@ client.on("message", (message) => {
     .setThumbnail(`http://i.imgur.com/bt9OsRs.jpg`)
     message.channel.send({ embed: embed });
   }
+   if(message.content.toLowerCase().startsWith(preefix + "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
    if (message.content.toLowerCase().startsWith(prefix + `commands`)) {
     const embed = new Discord.RichEmbed()
     .setTitle(`:page_with_curl: ツ Ticket Bot ツ Commands List :page_with_curl:`)
