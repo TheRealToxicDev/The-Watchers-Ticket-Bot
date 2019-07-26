ce47c8f0bb874a5a3bcbc38324236baf39489061
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-07-25 23:55:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -327,7 +327,16 @@ if (message.content.toLowerCase().startsWith(prefix + `version`)) {
           }, 3000);
         });
     });
-  })
+  }
+  if (message.content.toLowerCase().startsWith(prefix + `say`)) {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  }
 
 });
 // THE CLIENT LOGIN PROCESS (BOT READS THE TOKEN FROM HERE)
