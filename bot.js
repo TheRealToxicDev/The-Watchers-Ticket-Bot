9666fbc280ac799d82cc9ae88ed8386f397b4b17
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-12-07 01:31:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,6 +59,16 @@ client.on('guildMemberRemove', member => {
   client.channels.get(serverStats.botCountID).setName(`Bots: ${member.guild.members.filter(m => m.user.bot).size}`);
 });
 
+// Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+
 client.on("message", (message) => {
   if (message.guild.id !== serverStats.guildID) return;
   if (!message.content.startsWith(prefix) || message.author.bot) return;
