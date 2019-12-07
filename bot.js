3cf2a1e09fb057eb6085925a983be382455e8da1
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-12-07 15:10:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -134,11 +134,11 @@ if(message.member.roles.has(myRole.id)) {
   }
 
 if (command === "say") {
+    message.delete().catch()
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
     const sayEmbed = new Discord.RichEmbed()
     sayEmbed.setColor("#FF00FF")
