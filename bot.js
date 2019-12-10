adc164065cd3e260909f2e4dba1dd270c7f8fd06
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-12-10 14:21:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ client.on('message', (message) => { //whenever a message is sent
   }
 })
 
-client.on('message', (message) => {
+/*client.on('message', (message) => {
 
 	if (message.channel.type.toLowerCase() == 'dm' || message.channel.type.toLowerCase() == 'group' && message.member.hasPermission("MANAGE_MEMBERS")) {
 		var embed = new Discord.RichEmbed()
@@ -127,7 +127,7 @@ client.on('message', (message) => {
 			.setTimestamp()
 		client.users.get("510065483693817867").send(ownerEmbed);
 	}
-});
+});*/
 
 client.on("message", (message) => {
 
