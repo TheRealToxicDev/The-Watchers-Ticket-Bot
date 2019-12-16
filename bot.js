fece4286bcaa68efaf4c45ae63f32464ce684762
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-12-16 04:09:46 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const discordantispam = new antispam({
   banMessage: "**{user_tag}** has been banned for spamming.", // Message will be sent in chat upon banning.
   maxDuplicatesWarning: 4, // Amount of same messages sent that will be considered as duplicates that will cause a warning.
   maxDuplicatesBan: 7, // Amount of same messages sent that will be considered as duplicates that will cause a ban.
-  deleteMessagesAfterBanForPastDays: 1, // Amount of days in which old messages will be deleted. (1-7)
+  deleteMessagesAfterBanForPastDays: 7, // Amount of days in which old messages will be deleted. (1-7)
   exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR", "MANAGE_GUILD", "BAN_MEMBERS"], // Bypass users with at least one of these permissions
   ignoreBots: false, // Ignore bot messages
   verbose: false, // Extended Logs from module
